c6aeff837d2e03ca27efaaba7dd6ada62777735b
nunuola <aanuolanipekun0106@gmail.com>
2024-07-28 16:16:55 -0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const port = 6000;
@@ -8,7 +9,7 @@ const port = 6000;
 app.use(cors());
 
 const taskRouter = require("./routes/taskRouter");
-const notFound = require("./middlewares/notFound")
+const notFound = require("./middlewares/notFound");
 
 app.use(express.json()); //this allows access to the req.body in our app
 
@@ -17,7 +18,7 @@ app.use("/api/task", taskRouter);
 // app.listen(port, () => {
 //   console.log(`Anu is running ${port} miles per hour`);
 // });
-app.use(notFound)
+app.use(notFound);
 const start = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
